1096ae38c0c00511c06dd3e4518133dd7c196104
Alberto Chávez Pérez <alberto@francisco.nu>
2020-07-15 17:00:52 +0200

    Material-UI inst

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import UserForm from './components/UserForm';
+import Button from '@material-ui/core/Button'
+
 
 function App() {
   return (
     <div className="App">
 			<UserForm />
+
+      <br />
+      <Button variant="contained" color="primary">
+        Hello
+      </Button>
+
+
     </div>
   );
 }
