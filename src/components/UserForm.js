c08c4c6552fd0d5deae4fe340d62b86be22f6aec
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2020-07-16 16:44:52 +0200

    Form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -4,8 +4,38 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import Avatar from "@material-ui/core/Avatar";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  paper: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    marginTop: "100px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "50px",
+    borderRadius: "10px",
+  },
+  avatar: {
+    background:
+      "linear-gradient(45deg, rgb(33, 150, 243) 30%, rgb(33, 203, 243) 90%)",
+    margin: "10px",
+  },
+  form: {
+    margin: "20px",
+  },
+  submitButton: {
+    marginTop: "20px",
+  },
+});
 
 function UserForm() {
+  const classes = useStyles();
+
   // Destructure the array returned from the custom hook
   const [firstName, bindFirstName, resetFirstName] = useInput("");
   const [lastName, bindLastName, resetLastName] = useInput("");
@@ -15,23 +45,33 @@ function UserForm() {
     e.preventDefault();
 
     if (!checked) {
-      alert('First check the checkbox!')
-      return
+      alert("First check the checkbox!");
+      return;
     }
 
     alert(`Hello ${firstName} ${lastName}`);
     resetFirstName();
     resetLastName();
+    setChecked(false)
   };
 
   return (
-    <>
-      <form>
+    <Container component="main" maxWidth="xs" className={classes.paper}>
+      <Avatar className={classes.avatar}>
+        <ChatBubbleOutlineIcon />
+      </Avatar>
+
+      <Typography component="h1" variant="h5">
+        Fire an alert
+      </Typography>
+
+      <form className={classes.form}>
         <div>
           <TextField
-            id="outlined-basic"
             label="First Name"
             variant="filled"
+            margin="normal"
+            fullWidth
             {...bindFirstName}
           />
 
@@ -44,7 +84,13 @@ function UserForm() {
         </div>
 
         <div>
-          <TextField label="Last name" variant="filled" {...bindLastName} />
+          <TextField
+            label="Last name"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            {...bindLastName}
+          />
 
           <FormControlLabel
             control={
@@ -53,18 +99,26 @@ function UserForm() {
                 name="understood"
                 value="The string to use as the value of the checkbox when submitting the form, if the checkbox is currently toggled on"
                 color="primary"
-                onClick={(e) => {console.log(e); setChecked(!checked)}}
+                onClick={(e) => {
+                  setChecked(!checked);
+                }}
               />
             }
-            label="I understand I will trigger an alert"
+            label="I understand that I will trigger an alert"
           />
 
-          <Button variant="contained" color="primary" onClick={submitHandler}>
+          <Button
+            className={classes.submitButton}
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={submitHandler}
+          >
             Submit
           </Button>
         </div>
       </form>
-    </>
+    </Container>
   );
 }
 
